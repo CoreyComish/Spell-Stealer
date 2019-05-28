c5cfd9c22f5aa551e57c57d4bfd1772b9346bfca
Corey Comish <ccomish@uoregon.edu>
2019-05-28 13:13:19 -0700

    Fixed destroybyboundary, change to destroybyenv, note the env must be tagged for this to work

---
+++ Unity/Assets/Scripts/Projectiles/DestroyByEnv.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByEnv : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Env")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
