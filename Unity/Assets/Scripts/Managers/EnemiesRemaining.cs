40347038ab7059807fcd135b6bff2551581af3ab
Corey Comish <ccomish@uoregon.edu>
2019-05-29 13:11:51 -0700

    Progress on game progression manager

--- Unity/Assets/Scripts/Managers/EnemiesRemaining.cs
+++ Unity/Assets/Scripts/Managers/EnemiesRemaining.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class EnemiesRemaining : MonoBehaviour
 {
     private int eRem;
+    private GameProgression gp;
     Text text;
+    public string sceneName;
 
     void Awake()
     {
         text = GetComponent<Text>();
+        sceneName = SceneManager.GetActiveScene().name;
     }
 
     void Update()
@@ -18,5 +22,10 @@ public class EnemiesRemaining : MonoBehaviour
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
         eRem = enemies.Length;
         text.text = "Enemies Remaining: " + eRem;
+        if (eRem == 0)
+        {
+            print(sceneName);
+            gp.LevelComplete(sceneName);
+        }
     }
 }
---
+++ Unity/Assets/Scripts/Managers/GameProgression.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameProgression
+{
+    public List<string> completedLevels;
+
+    // Load back into Hub level, if all levels completed open boss room
+    public void LevelComplete(string level)
+    {
+        if (!completedLevels.Contains(level))
+        {
+            completedLevels.Add(level);
+        }
+        if (completedLevels.Count == 3)
+        {
+            // Return to Hub level, open boss room
+            // TO:DO Congrats Message
+            SceneManager.LoadScene("Level00_HubBoss");
+        }
+        if (completedLevels.Count == 4)
+        {
+            // Player beat game, roll credits, return to main menu
+            // TO:DO Congrats Message
+            SceneManager.LoadScene("MainMenu");
+        }
+        else
+        {
+            // Return to Hub level
+            // TO:DO Congrats Message
+            SceneManager.LoadScene("Level00_Hub");
+        }
+    }
+
+    // Load back into Hub level, if boss room is open keep open
+    public void LevelDeath(string level)
+    {
+        if (completedLevels.Count == 3)
+        {
+            // Return to Hub Level, open boss room
+            // TO:DO Death Message
+            SceneManager.LoadScene("Level00_HubBoss");
+        }
+        else
+        {
+            // Return to Hub Level
+            // TO:DO Death Message
+            SceneManager.LoadScene("Level00_Hub");
+        }
+    }
+}
